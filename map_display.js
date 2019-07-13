cc39c28630ed5adce2f611f0f8e4705258e30e9a
tizuemon <36941566+tizuemon@users.noreply.github.com>
2019-07-13 11:55:41 +0900

    Add files via upload

--- map_display.js
+++ map_display.js
@@ -30,11 +30,13 @@ L.control.layers(Map_BaseLayer, null, {
     collapsed: true
 }).addTo(map)
 
+L.control.scale({imperial:false}).addTo(map);
 
-var occur_info = []; // マーカー情報を格納する配列
-var occur_contents = [];  // ポップアップで表示する内容
-var occur_lat = [];  // 緯度
-var occur_lon = [];  // 経度
+
+var occur_info = [];      // 不審者出没マーカー情報
+var occur_contents = [];  // 不審者出没表示内容
+var occur_lat = [];  // 不審者出没緯度
+var occur_lon = [];  // 不審者出没経度
 
 var safeguard_info = [];     // まもるいえマーカー情報
 var safeguard_contents = []; // まもるいえ表示内容
@@ -42,8 +44,50 @@ var safeguard_lat = [];  // まもるいえ緯度
 var safeguard_lon = [];  // まもるいえ経度
 var safeguard_img = [];  // まもるいえ写真
 
-$.ajaxSetup({ async: false });   //同期通信(json取ってくるまで待つ)
-$.getJSON("map_data.json", function (data) {
+var occur_flag = 0;
+var safeguard_flag = 0;
+var danger_flag = 0;
+
+var occur_tile;
+var safeguard_tile;
+var danger_tile = [];
+
+var occur_popOpt = {
+    'className': 'occur_pop'
+}
+var safeguard_popOpt = {
+    'className': 'safeguard_pop'
+}
+var occur_pop_title = '<div class="occur_pop_title">不審者出没地点</div>';
+var safeguard_pop_title = '<div class="safeguard_pop_title">こどもをまもるいえ</div>';
+
+$.getJSON({ url: 'map_data.json', cache: false, }) // json読み込み開始
+    .done(function (data) { // jsonの読み込みに成功した時
+        console.log('成功');
+        len = data.length;
+        for (var i = 0; i < len; i++) {
+            if (data[i].kind == 0) {
+                occur_contents.push(data[i].time)
+                occur_lat.push(data[i].lat);
+                occur_lon.push(data[i].lon);
+            }
+            else{
+                safeguard_contents.push(data[i].time)
+                safeguard_lat.push(data[i].lat);
+                safeguard_lon.push(data[i].lon);
+                safeguard_img.push(data[i].img);
+            }
+        }
+        data_load();
+    })
+    .fail(function () { // jsonの読み込みに失敗した時
+        console.log('失敗');
+    }
+);
+
+/*
+function get_data(data){
+    console.log('成功');
     len = data.length;
     for (var i = 0; i < len; i++) {
         if (data[i].kind == 0) {
@@ -58,47 +102,44 @@ $.getJSON("map_data.json", function (data) {
             safeguard_img.push(data[i].img);
         }
     }
-});
-$.ajaxSetup({ async: true });
-
-// GeoJSON形式で複数個のマーカーを設定する
-// 不審者出没
-for (var i = 0; i < occur_lat.length; i++) {
-    occur_info.push({     // 1つのマーカーの情報を格納する
-        "type": "Feature",
-        "properties": {
-            "popupContent": occur_contents[i]
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [occur_lon[i], occur_lat[i]]
-        }
-    });
+    data_load();
+    occur_appear();
+    safeguard_appear();
+    danger_appear();
 }
+*/
 
-// GeoJSON形式で複数個のマーカーを設定する
-// こどもをまもるいえ
-for (var i = 0; i < safeguard_lat.length; i++) {
-    safeguard_info.push({     // 1つのマーカーの情報を格納する
-        "type": "Feature",
-        "properties": {
-            "popupContent": safeguard_contents[i] + "<br><br><img src=" + safeguard_img[0] + "width='200' height='200'></img>"
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [safeguard_lon[i], safeguard_lat[i]]
-        }
-    });
+function data_load(){
+    // GeoJSON形式で複数個のマーカーを設定する
+    // 不審者出没
+    for (var i = 0; i < occur_lat.length; i++) {
+        occur_info.push({     // 1つのマーカーの情報を格納する
+            "type": "Feature",
+            "properties": {
+                "popupContent": occur_contents[i]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [occur_lon[i], occur_lat[i]]
+            }
+        });
+    }
+
+    // こどもをまもるいえ
+    for (var i = 0; i < safeguard_lat.length; i++) {
+        var img_info = "<br><img src=" + safeguard_img[0] + "width='100%' height='auto' text-aling='center'></img>";
+        safeguard_info.push({     // 1つのマーカーの情報を格納する
+            "type": "Feature",
+            "properties": {
+                "popupContent": safeguard_contents[i] + img_info
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [safeguard_lon[i], safeguard_lat[i]]
+            }
+        });
+    }
 }
-
-var occur_flag = 0;
-var safeguard_flag = 0;
-var danger_flag = 0;
-
-var occur_tile;
-var safeguard_tile;
-var danger_tile = [];
-// var danger_tile = L.layerGroup();
 
 // 不審者出没表示
 function occur_appear() {
@@ -113,16 +154,16 @@ function occur_appear() {
             {
                 onEachFeature: function (feature, layer) {
                     if (feature.properties && feature.properties.popupContent) {
-                        layer.bindPopup(feature.properties.popupContent);
+                        layer.bindPopup(occur_pop_title + feature.properties.popupContent, occur_popOpt);  // popupOptions : class名を振る
                     }
                 },
                 // オリジナル画像を設定する
                 pointToLayer: function (feature, latlng) {
                     var myIcon = L.icon({
-                        iconUrl: 'marker-red.png',  // 画像のURI
+                        iconUrl: 'img/marker-red.png',  // 画像のURI
                         iconSize: [25, 41],         // 画像のサイズ設定
                         iconAnchor: [12, 40],       // 画像の位置設定
-                        popupAnchor: [0, -40]       //　　ポップアップの表示を開始する位置設定
+                        popupAnchor: [0, -40]       // ポップアップの表示を開始する位置設定
                     });
                     return L.marker(latlng, { icon: myIcon });  // マーカーに画像情報を設定
                 }
@@ -150,13 +191,13 @@ function safeguard_appear() {
             {
                 onEachFeature: function (feature, layer) {
                     if (feature.properties && feature.properties.popupContent) {
-                        layer.bindPopup(feature.properties.popupContent);
+                        layer.bindPopup(safeguard_pop_title + feature.properties.popupContent, safeguard_popOpt);
                     }
                 },
                 // オリジナル画像を設定する
                 pointToLayer: function (feature, latlng) {
                     var myIcon = L.icon({
-                        iconUrl: 'mamoruie.png',  // 画像のURI
+                        iconUrl: 'img/mamoruie.png',  // 画像のURI
                         iconSize: [40, 40],         // 画像のサイズ設定
                         iconAnchor: [12, 40],       // 画像の位置設定
                         popupAnchor: [0, -40]       //　　ポップアップの表示を開始する位置設定
@@ -183,29 +224,13 @@ function danger_appear() {
         danger_flag = 1;
 
         for (var i = 0; i < occur_lat.length; i ++ ){
-            danger_tile[i] = L.circle([occur_lat[i], occur_lon[i]], 500,{ // 位置と半径
+            danger_tile[i] = L.circle([occur_lat[i], occur_lon[i]], 200,{ // 位置と半径
                 // radius: 1000,
                 color: 'blue',
                 fillColor: '#399ade',
                 fillOpacity: 0.5,
                 weight: 0  // 枠線の太さ
             }).addTo(map);
-
-            /*
-            danger_tile.addLayer(L.circle([lat[i], lon[i]], 500,{
-                // radius: 1000,
-                color: 'blue',
-                fillColor: '#399ade',
-                fillOpacity: 0.5,
-                weight: 0  // 枠線の太さ
-            }));
-            
-            var overlay = {
-                "Circle": danger_tile,
-            }
-            //layersコントロールのオーバーレイに設定
-            L.control.layers(null, overlay).addTo(map);
-            */
         }
     }
 }
